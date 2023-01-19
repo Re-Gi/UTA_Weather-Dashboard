cec78a73645d7dc9a03c447a535b042d1b2ac861
Rebecca Girndt <r.l.girndt@gmail.com>
2023-01-18 19:37:03 -0600

    fetched current weather with coordinates

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 //get coordinates: http://api.openweathermap.org/geo/1.0/direct?q={city}}&limit=1&appid=07220a51e95b28fddb66e8043de4c734
 //get current weather: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=07220a51e95b28fddb66e8043de4c734
 //get 5-day forecast: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=07220a51e95b28fddb66e8043de4c734
+// weather icon url: https://openweathermap.org/img/wn/{icon}.png
 
 var searchBtn = document.querySelector('.btn-primary');
 var inputEl = document.querySelector('input');
@@ -18,10 +19,33 @@ function getCoordinates() {
     })
     .then((geoRes) => {
         console.log(geoRes);
+        getCurrentWeather(geoRes);
+        //getForecast(geoRes);
     })
     .catch((error) => {
         console.error(error);
     });
 }
+
+function getCurrentWeather(geoRes) {
+    var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?lat=" + geoRes[0].lat + "&lon=" + geoRes[0].lon + "&appid=07220a51e95b28fddb66e8043de4c734";
+
+    fetch(currentWeatherAPI)
+    .then((response) => {
+        if(!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
+    .then((cwRes) => {
+        console.log(cwRes);
+        displayWeather(cwRes);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+function displayWeather() {}
 
 searchBtn.addEventListener('click', getCoordinates)
