3089abd2d14d51012f6d3066678d242082a89b40
Rebecca Girndt <r.l.girndt@gmail.com>
2023-01-18 18:59:04 -0600

    created desired page layout with html and css

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+//get coordinates: http://api.openweathermap.org/geo/1.0/direct?q={city}}&limit=1&appid=07220a51e95b28fddb66e8043de4c734
+//get current weather: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=07220a51e95b28fddb66e8043de4c734
+//get 5-day forecast: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=07220a51e95b28fddb66e8043de4c734
