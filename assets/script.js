cbb93a7442800821120d9709b6dbfa9ab1438c12
Rebecca Girndt <r.l.girndt@gmail.com>
2023-01-18 21:35:16 -0600

    built display function for current weather div

--- assets/script.js
+++ assets/script.js
@@ -19,52 +19,80 @@ function getCoordinates() {
     })
     .then((geoRes) => {
         console.log(geoRes);
-        // getCurrentWeather(geoRes);
-        getForecast(geoRes);
+        getCurrentWeather(geoRes);
+        // getForecast(geoRes);
     })
     .catch((error) => {
         console.error(error);
     });
 }
 
-// function getCurrentWeather(geoRes) {
-//     var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?lat=" + geoRes[0].lat + "&lon=" + geoRes[0].lon + "&appid=07220a51e95b28fddb66e8043de4c734";
+function getCurrentWeather(geoRes) {
+    var currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?lat=" + geoRes[0].lat + "&lon=" + geoRes[0].lon + "&appid=07220a51e95b28fddb66e8043de4c734&units=imperial";
 
-//     fetch(currentWeatherAPI)
-//     .then((response) => {
-//         if(!response.ok) {
-//             throw response.json();
-//         }
-//         return response.json();
-//     })
-//     .then((cwRes) => {
-//         console.log(cwRes);
-//         displayWeather(cwRes);
-//     })
-//     .catch((error) => {
-//         console.error(error);
-//     });
-// }
-
-function getForecast(geoRes) {
-    var forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?lat=" + geoRes[0].lat + "&lon=" + geoRes[0].lon + "&appid=07220a51e95b28fddb66e8043de4c734";
-
-    fetch(forecastAPI)
+    fetch(currentWeatherAPI)
     .then((response) => {
         if(!response.ok) {
             throw response.json();
         }
         return response.json();
     })
-    .then((fcRes) => {
-        console.log(fcRes);
-        // displayWeather(fcRes);
+    .then((cwRes) => {
+        console.log(cwRes);
+        displayCurrentWeather(cwRes);
     })
     .catch((error) => {
         console.error(error);
     });
 }
 
-function displayWeather(cwRes, fcRes) {}
+// function getForecast(geoRes) {
+//     var forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?lat=" + geoRes[0].lat + "&lon=" + geoRes[0].lon + "&appid=07220a51e95b28fddb66e8043de4c734";
+
+//     fetch(forecastAPI)
+//     .then((response) => {
+//         if(!response.ok) {
+//             throw response.json();
+//         }
+//         return response.json();
+//     })
+//     .then((fcRes) => {
+//         console.log(fcRes);
+//         displayForecast(fcRes);
+//     })
+//     .catch((error) => {
+//         console.error(error);
+//     });
+// }
+
+function displayCurrentWeather(cwRes) {
+    var weatherDiv = document.createElement('div');
+    weatherDiv.setAttribute('class', 'card-body');
+    weatherDiv.setAttribute('id', 'weather-div');
+
+    var cityNameEl = document.createElement('h2');
+    cityNameEl.setAttribute('class', 'city-name');
+
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+
+    cityNameEl.textContent = cwRes.name + " (" + dayjs.unix(cwRes.dt).format('M/DD/YYYY') + ") ";
+    tempEl.textContent = "Temp: " + cwRes.main.temp + "°F";
+    windEl.textContent = "Wind: " + cwRes.wind.speed + " MPH";
+    humidityEl.textContent = "Humidity: " + cwRes.main.humidity + " %";
+
+    var weatherResDiv = document.querySelector('#weather-results');
+    
+    weatherDiv.appendChild(cityNameEl);
+    weatherDiv.appendChild(tempEl);
+    weatherDiv.appendChild(windEl);
+    weatherDiv.appendChild(humidityEl);
+    weatherResDiv.appendChild(weatherDiv);
+}
+
+// function displayForecast(fcRes) {
+//     console.log(fcRes);
+// }
 
 searchBtn.addEventListener('click', getCoordinates)
